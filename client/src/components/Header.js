fd3b672a9ef169f9becc9d15015c9467f4d97c25
Muhammed <Muhammed.1319053@gmail.com>
2025-05-16 20:32:10 +0300

    Update Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,38 +9,59 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 function Header() {
     return (
         <header className="header">
-            <div className="top-bar">
-                <img src={require('../assets/images/logo.png')} alt="Your Logo" style={{ width: '60px', height: '60px' }} />
-                <div className="search-bar">
-                    <input type="text" placeholder="Search Products Here ..." />
-                    <button><SearchIcon /></button>
-                </div>
-                <div className="icons">
-                    <Link to="/favorites">
-                        <div className="icon-item">
-                            <span><FavoriteIcon /></span>
-                            <p>Favorites</p>
-                        </div>
-                    </Link>
-                    <Link to="/profile">
-                        <div className="icon-item">
-                            <span><AccountCircleIcon /></span>
-                            <p>Account</p>
-                        </div>
-                    </Link>
-                    <Link to="/cart">
-                        <div className="icon-item">
-                            <span><ShoppingCartIcon /></span>
-                            <p>Cart</p>
-                        </div>
-                    </Link>
-                </div>
+<div className="top-bar">
+    <img src={require('../assets/images/logo.png')} alt="Your Logo" style={{ width: '60px', height: '60px' }} />
+
+    {/* 6 Icon Buttons */}
+    <div className="mid-icons">
+        <button className="icon-button" >
+            <img src={require('../assets/images/acer.png')} alt="Icon 1" />
+        </button>
+        <button className="icon-button" >
+            <img src={require('../assets/images/apple.png')} alt="Icon 2" />
+        </button>
+        <button className="icon-button" >
+            <img src={require('../assets/images/asus.png')} alt="Icon 3" />
+        </button>
+        <button className="icon-button" >
+            <img src={require('../assets/images/dell.png')} alt="Icon 4" />
+        </button>
+        <button className="icon-button" >
+            <img src={require('../assets/images/hp.png')} alt="Icon 5" />
+        </button>
+        <button className="icon-button" >
+            <img src={require('../assets/images/lenovo.png')} alt="Icon 6" />
+        </button>
+    </div>
+
+    {/* Icons to the right */}
+    <div className="icons">
+        <Link to="/favorites">
+            <div className="icon-item">
+                <span><FavoriteIcon /></span>
+                <p>Favorites</p>
             </div>
+        </Link>
+        <Link to="/profile">
+            <div className="icon-item">
+                <span><AccountCircleIcon /></span>
+                <p>Account</p>
+            </div>
+        </Link>
+        <Link to="/cart">
+            <div className="icon-item">
+                <span><ShoppingCartIcon /></span>
+                <p>Cart</p>
+            </div>
+        </Link>
+    </div>
+</div>
+
             <nav className="nav-bar">
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/Services">Services</Link></li>
-                    <li><a href="#shop">Shop</a></li>
+                    <li><Link to="/home#shop">Shop</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
